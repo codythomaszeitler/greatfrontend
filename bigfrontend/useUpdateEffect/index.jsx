57440b1bd5efb0cd1b5e4b3bcb6bd9f51292a4dd
Cody Thomas Zeitler <codyzeitler12@gmail.com>
2026-02-16 11:47:54 -0700

    fix: get hello world onto the screen

--- bigfrontend/useUpdateEffect/index.jsx
+++ bigfrontend/useUpdateEffect/index.jsx
@@ -1,6 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
 import {createRoot} from 'react-dom/client';
 
 document.body.innerHTML = '<div id="app"></div>'
 
 const root = createRoot(document.getElementById('app'));
-// root.render(<h1>Hello, World</h1>)
+root.render(<h1>Hello, World</h1>)
+
+console.log('we are within index.jsx');
---
+++ bigfrontend/useUpdateEffect/webpack.config.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const path = require('path');
+
+module.exports = {
+    entry: './index.jsx',
+    context: path.resolve(__dirname),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.m?jsx$/,
+                exclude : /node_modules/,
+                use : {
+                    loader : "babel-loader",
+                    options : {
+                        presets : ["@babel/preset-env", "@babel/preset-react"]
+                    }
+                }
+            }
+        ]
+    },
+    resolve: {
+    },
+    devtool: 'source-map',
+    plugins: [
+    ]
+};
